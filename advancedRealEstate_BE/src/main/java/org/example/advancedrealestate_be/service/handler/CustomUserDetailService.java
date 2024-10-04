4a37766c1454425b5e1dea0d47ac64f0bb63af7b
ngockhanhnktech <nguyenngockhanhtech@gmail.com>
2024-10-04 09:57:07 +0700

    update

--- advancedRealEstate_BE/src/main/java/org/example/advancedrealestate_be/service/handler/CustomUserDetailService.java
+++ advancedRealEstate_BE/src/main/java/org/example/advancedrealestate_be/service/handler/CustomUserDetailService.java
@@ -1,56 +1,52 @@
-package org.example.advancedrealestate_be.service.handler;
-
-
-import org.example.advancedrealestate_be.dto.MyUserDetail;
-import org.example.advancedrealestate_be.dto.RoleDto;
-import org.example.advancedrealestate_be.dto.UserDto;
-import org.example.advancedrealestate_be.service.UserService;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Service
-public class CustomUserDetailService implements UserDetailsService {
-
-    @Autowired
-    private UserService userService;
-
+//package org.example.advancedrealestate_be.service.handler;
+//
+//
+//import org.example.advancedrealestate_be.dto.MyUserDetail;
+//import org.example.advancedrealestate_be.dto.RoleDto;
+//import org.example.advancedrealestate_be.dto.UserDto;
+//import org.example.advancedrealestate_be.service.UserService;
+//import org.springframework.beans.BeanUtils;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//import org.springframework.stereotype.Service;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Service
+//public class CustomUserDetailService implements UserDetailsService {
+//
+//    @Autowired
+//    private UserService userService;
+//
+////    @Override
+////    public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
+////        UserDto userDTO = userService.findOneByUserNameAndStatus(name, 1);
+////        if(userDTO == null){
+////            throw new UsernameNotFoundException("Username not found");
+////        }
+////        List<GrantedAuthority> authorities = new ArrayList<>();
+////        for(RoleDto role: userDTO.getRoles()){
+////            authorities.add(new SimpleGrantedAuthority("ROLE_"+role.getCode()));
+////        }
+////        MyUserDetail myUserDetail = new MyUserDetail(name,userDTO.getPassword(),true,true,true,true,authorities);
+////        BeanUtils.copyProperties(userDTO, myUserDetail);
+////        return myUserDetail;
+////    }
+//
 //    @Override
 //    public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
 //        UserDto userDTO = userService.findOneByUserNameAndStatus(name, 1);
-//        if(userDTO == null){
+//        if (userDTO == null) {
 //            throw new UsernameNotFoundException("Username not found");
 //        }
 //        List<GrantedAuthority> authorities = new ArrayList<>();
-//        for(RoleDto role: userDTO.getRoles()){
-//            authorities.add(new SimpleGrantedAuthority("ROLE_"+role.getCode()));
+//        for (RoleDto role : userDTO.getRoles()) {
+//            authorities.add(new SimpleGrantedAuthority("ROLE_" + role.getCode()));
 //        }
-//        MyUserDetail myUserDetail = new MyUserDetail(name,userDTO.getPassword(),true,true,true,true,authorities);
+//        MyUserDetail myUserDetail = new MyUserDetail(name, userDTO.getPassword(), true, true, true, true, authorities);
 //        BeanUtils.copyProperties(userDTO, myUserDetail);
 //        return myUserDetail;
 //    }
-
-    @Override
-    public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
-        UserDto userDTO = userService.findOneByUserNameAndStatus(name, 1);
-        if (userDTO == null) {
-            throw new UsernameNotFoundException("Username not found");
-        }
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        for (RoleDto role : userDTO.getRoles()) {
-            authorities.add(new SimpleGrantedAuthority("ROLE_" + role.getCode()));
-        }
-        MyUserDetail myUserDetail = new MyUserDetail(name, userDTO.getPassword(), true, true, true, true, authorities);
-        BeanUtils.copyProperties(userDTO, myUserDetail);
-        return myUserDetail;
-    }
-
-}
+//
+//}
