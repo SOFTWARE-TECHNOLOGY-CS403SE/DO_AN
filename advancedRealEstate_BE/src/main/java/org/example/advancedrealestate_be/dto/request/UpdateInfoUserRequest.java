a6b94191b301cf3d3a7980e2e11c4eacd9d2ce83
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2024-10-17 14:49:49 +0700

    refactor: add update user info request

---
+++ advancedRealEstate_BE/src/main/java/org/example/advancedrealestate_be/dto/request/UpdateInfoUserRequest.java
@@ -0,0 +1,12 @@
+package org.example.advancedrealestate_be.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class UpdateInfoUserRequest {
+
+    String firstName;
+    String lastName;
+
+
+}
